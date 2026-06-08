224874d719a7cf5a9d2518622490f405cc155b02
Aayush <aayushahir4169yadav@gmail.com>
2026-06-08 18:30:05 +0530

    implment head footer and images in card

--- models/listing.js
+++ models/listing.js
@@ -11,10 +11,10 @@ const ListingSchema = mongoose.Schema({
   image: {
     type: String,
     default:
-      "https://unsplash.com/photos/wildflower-meadow-with-a-lush-green-forest-under-blue-sky-Zz1DmaAD_Qs",
+      "https://images.unsplash.com/photo-1780380069585-4703916d9ea4?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     set: (v) =>
       v === ""
-        ? "https://unsplash.com/photos/wildflower-meadow-with-a-lush-green-forest-under-blue-sky-Zz1DmaAD_Qs"
+        ? "https://images.unsplash.com/photo-1780380069585-4703916d9ea4?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         : v,
   },
   price: {
